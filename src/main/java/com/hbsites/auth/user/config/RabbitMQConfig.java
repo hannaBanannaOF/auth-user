09310f4ef7fe9146ff7a682868d10fd9bfe78883
Ravi Batistel <ravi.batistel@outlook.com>
2023-07-06 10:11:10 -0300

    RabbitMQ fixes

--- src/main/java/com/hbsites/auth/user/config/RabbitMQConfig.java
+++ src/main/java/com/hbsites/auth/user/config/RabbitMQConfig.java
@@ -4,6 +4,9 @@ import org.springframework.amqp.core.Binding;
 import org.springframework.amqp.core.BindingBuilder;
 import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.core.TopicExchange;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -12,43 +15,45 @@ public class RabbitMQConfig {
     public static final String USER_REQUEST_QUEUE = "user.request";
     public static final String USER_RESPONSE_QUEUE = "user.response";
     public static final String USER_EXCHANGE = "user-exchange";
-    /** *
-     * Configure the Send Message Queue
-     */
+
     @Bean
     Queue msgQueue() {
         return new Queue(USER_REQUEST_QUEUE);
     }
-    /** *
-     * Return Queue Configuration
-     */
+
     @Bean
     Queue replyQueue() {
         return new Queue(USER_RESPONSE_QUEUE);
     }
-    /** *
-     * Switch setting
-     */
+
     @Bean
     TopicExchange topicExchange() {
         return new TopicExchange(USER_EXCHANGE);
     }
-    /** *
-     * Queuing and Switch Link Request
-     */
+
     @Bean
     Binding msgBinding() {
         return BindingBuilder.bind(msgQueue())
                 .to(topicExchange())
                 .with(USER_REQUEST_QUEUE);
     }
-    /** *
-     * Back to Queue and Switch Link
-     */
+
     @Bean
     Binding replyBinding() {
         return BindingBuilder.bind(replyQueue())
                 .to(topicExchange())
                 .with(USER_RESPONSE_QUEUE);
+    }
+
+    @Bean
+    public RabbitTemplate rabbitTemplate(final ConnectionFactory connectionFactory) {
+        final var rabbitTemplate = new RabbitTemplate(connectionFactory);
+        rabbitTemplate.setMessageConverter(producerJackson2MessageConverter());
+        return rabbitTemplate;
+    }
+
+    @Bean
+    public Jackson2JsonMessageConverter producerJackson2MessageConverter() {
+        return new Jackson2JsonMessageConverter();
     }
 }
--- src/main/java/com/hbsites/auth/user/listener/UserRequestListener.java
+++ src/main/java/com/hbsites/auth/user/listener/UserRequestListener.java
@@ -2,9 +2,9 @@ package com.hbsites.auth.user.listener;
 
 import com.hbsites.auth.user.config.RabbitMQConfig;
 import com.hbsites.auth.user.service.KeycloakService;
-import com.hbsites.hbsitescommons.dto.UUIDListPayload;
+import com.hbsites.hbsitescommons.messages.UUIDListPayload;
 import com.hbsites.hbsitescommons.dto.UserDTO;
-import com.hbsites.hbsitescommons.dto.UserDTOListPayload;
+import com.hbsites.hbsitescommons.messages.UserDTOListPayload;
 import org.springframework.amqp.core.Message;
 import org.springframework.amqp.core.MessageBuilder;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
@@ -13,10 +13,10 @@ import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.utils.SerializationUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Lazy;
+import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
-import java.util.UUID;
 
 @Component
 public class UserRequestListener {
@@ -29,14 +29,12 @@ public class UserRequestListener {
     private RabbitTemplate rabbitTemplate;
 
     @RabbitListener(queues = RabbitMQConfig.USER_REQUEST_QUEUE)
-    public void process(Message message) {
-        byte[] body = message.getBody();
-        UUIDListPayload userIds = (UUIDListPayload) SerializationUtils.deserialize(body);
-        List<UserDTO> users = keycloakService.getPlayerByUuid(userIds.getUuids());
+    public void process(UUIDListPayload message) {
+        List<UserDTO> users = keycloakService.getPlayerByUuid(message.uuids());
 
         //This is the message to be returned by the server
-        Message build = MessageBuilder.withBody(SerializationUtils.serialize(new UserDTOListPayload(users))).build();
-        CorrelationData correlationData = new CorrelationData(message.getMessageProperties().getCorrelationId());
-        rabbitTemplate.sendAndReceive(RabbitMQConfig.USER_EXCHANGE, RabbitMQConfig.USER_RESPONSE_QUEUE, build, correlationData);
+        UserDTOListPayload build = new UserDTOListPayload(users);
+//        CorrelationData correlationData = new CorrelationData();
+        rabbitTemplate.convertSendAndReceiveAsType(RabbitMQConfig.USER_EXCHANGE, RabbitMQConfig.USER_RESPONSE_QUEUE, build, new ParameterizedTypeReference<>(){});
     }
 }
